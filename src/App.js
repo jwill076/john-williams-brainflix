80532121cd3aeeece78efd8559f193343396d876
John Williams <jw@mlmpipa.org>
2022-05-15 23:19:18 -0400

    Initializing State in App.js Component

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.scss";
+import React from "react";
+import VideoDetailsData from "./data/video-details.json";
+import Videos from "./data/videos.json";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      details: VideoDetailsData[0],
+      videos: Videos.filter(video => video.id !== VideoDetailsData[0].id)
+    }
+  }
+
+  render() {
+    return (
+      <>
+
+      </>
+    );
+  }
+}
+
+export default App;
