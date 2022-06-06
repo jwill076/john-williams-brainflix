c7f8b3c6a0a2bfbff2423eb9ca57d9b090ceba26
John Williams <jw@mlmpipa.org>
2022-06-05 20:07:50 -0400

    Updated Folder Structure, New Imports, New Page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import "./App.scss";
 import React from "react";
-import axios from 'axios';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Upload from "./components/Upload/Upload";
 import NotFound from "./components/NotFound/NotFound";
 
-function App() {
-    return (
-      <>
-        <BrowserRouter>
-          <Header />
-          
-          <Switch>
-            {/* <Redirect from="/home" to="/" /> */}
-            <Route path="/" exact component={Home} />
-            <Route path="/:id" componment={Home} />
-            <Route path="/upload" component={Upload} />
-            <Route component={NotFound} />
-          </Switch>
-        </BrowserRouter>
-      </>
-    );
-  }
-
-export default App;
