a4068edb8fd0b2d3b2b276b33a03839122363931
John Williams <jw@mlmpipa.org>
2022-06-05 20:20:35 -0400

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,3 +6,72 @@ import "../Header/Header.scss";
 import { NavLink } from 'react-router-dom';
 import React from "react";
 
+function Header() {
+    return (
+        <header className="header">
+            <article className="header__flexbox1">
+                <section className="flexbox1__logo-container">
+                    <NavLink to="/home" className="flexbox1__logo-link">
+                        <img
+                            className="flexbox1__logo"
+                            src={BrainFlixLogo}
+                            alt="BrainFlix Logo"
+                        />
+                    </NavLink>
+                </section>
+            </article>
+
+            <article className="header__flexbox2">
+                <section className="flexbox2__container1">
+                    <section className="container1__search-bar">
+                        <div className="container1__image-container">
+                            <img
+                                className="container1__image"
+                                src={MagnifyingGlassIcon}
+                                alt="Magnifying Glass Icon"
+                            />
+                        </div>
+
+                        <input
+                            className="container1__search"
+                            type="text"
+                            name="container1__search"
+                            id="container1__search"
+                            placeholder="Search"
+                        ></input>
+                    </section>
+
+                    <section className="container1__avatar-image">
+                        <img
+                            className="container1__image"
+                            src={MohanMurugeAvatar}
+                            alt="Mohan-muruge Avatar"
+                        />
+                    </section>
+                </section>
+
+                <article className="flexbox2__container2">
+                    <button className="container2__button">
+                        <section className="button__image-container">
+                            <img
+                                className="button__image"
+                                src={UploadIcon}
+                                alt="Upload Icon"
+                            />
+                        </section>
+                        
+                        <NavLink to="/upload" className="container2__link">
+                            <section className="button__text-container">
+                                <p className="button__text">
+                                    UPLOAD
+                                </p>
+                            </section>
+                        </NavLink>
+                    </button>    
+                </article>
+            </article>
+        </header>
+    )
+}
+
+export default Header
