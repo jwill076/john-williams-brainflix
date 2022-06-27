5ba20e0cae85e9f35217b108a523b29b560d248e
John Williams <jw@mlmpipa.org>
2022-06-26 23:23:23 -0400

    Tried to Fix NotFound Component Route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import React from "react";
 import Header from "./components/Header/Header";
 import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
 import Home from "../src/components/Home/Home";
-import Upload from "./pages/Upload/Upload";
-import NotFound from "./pages/NotFound/NotFound";
+import Upload from "../src/components/Upload/Upload";
+import NotFound from "../src/components/NotFound/NotFound";
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      
+      <Switch>
+        <Redirect from="/home" to="/" />
+        <Route path="/" exact component={Home} />
+        <Route path="/upload" component={Upload} />
+        <Route path="/:id" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
