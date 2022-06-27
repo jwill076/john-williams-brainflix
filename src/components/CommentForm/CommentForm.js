77a3997e2bb3bc95e1c04e821a61ccdf125ff4da
John Williams <jw@mlmpipa.org>
2022-06-26 23:32:28 -0400

    Connected Avatar Image Source to API Express Server

--- src/components/CommentForm/CommentForm.js
+++ src/components/CommentForm/CommentForm.js
@@ -1,4 +1,55 @@
+import MohanMurugeAvatar from "../../assets/images/Mohan-muruge.jpg";
 import AddCommentIcon from "../../assets/icons/add_comment.svg";
 import "../CommentForm/CommentForm.scss";
 import React from "react";
 
+function CommentForm() {
+    return (
+        <section className="flexbox2__jtc-comment-form">
+            <h3 className="jtc-comment-form__title">
+                JOIN THE CONVERSATION
+            </h3>
+
+            <div className="jtc-comment-form__imagebox">
+                <div className="imagebox__avatar-container">
+                    <img
+                        className="imagebox__avatar"
+                        src={MohanMurugeAvatar}
+                        alt="Mohan-muruge Avatar"
+                    />
+                </div>
+
+                <div className="imagebox__input-field">
+                    <input
+                        className="imagebox__input"
+                        type="text"
+                        name="comment-form__name"
+                        id="comment-form__name"
+                        placeholder="Add a new comment"
+                    ></input>
+                </div>
+            </div>
+
+            <div className="jtc-comment-form__add-comment">
+                <div className="add-comment__image-container">
+                    <img
+                        className="add-comment__image"
+                        src={AddCommentIcon}
+                        alt="Add Comment Icon"
+                    />
+                </div>
+
+                <div className="add-comment__button-container">
+                    <button
+                        className="add-comment__button"
+                        type="submit"
+                        id="add-comment__button">
+                            COMMENT
+                    </button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default CommentForm
